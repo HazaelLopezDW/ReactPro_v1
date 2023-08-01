921ab902b7a9885693e3d818d0be3801e68e18e6
Hazael Lopez <elhazael134@gmail.com>
2023-08-01 11:53:20 -0600

    Preparación de proyecto - React-App

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -10,13 +10,28 @@ export const Navigation = () => {
                     <img src={logo} alt='react logo'/>
                     <ul>
                         <li>
-                            <NavLink to='/home' className={({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink 
+                                to='/home' 
+                                className={({isActive}) => isActive ? 'nav-active' : '' }
+                            >
+                                Home
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/about' className={({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
+                            <NavLink 
+                                to='/about' 
+                                className={({isActive}) => isActive ? 'nav-active' : '' }
+                            >
+                                About
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/users' className={({isActive}) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                            <NavLink 
+                                to='/users' 
+                                className={({isActive}) => isActive ? 'nav-active' : '' }
+                            >
+                                Users
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
