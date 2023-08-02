f00ad09fcbae41c42822918cce026049dce70ab2
Hazael Lopez <elhazael134@gmail.com>
2023-08-02 15:23:38 -0600

    Nested Lazy Routes

---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const NoLazy = () => {
+  return (
+    <div>
+        <h1>No Lazyload Componente</h1>
+    </div>
+  )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -21,7 +21,7 @@ export const Navigation = () => {
                             <img src={logo} alt='react logo' />
                             <ul>
                                 {/* TODO: Crear NavLink dimanicos */}
-                                {routes.map(({to, name}) => (
+                                {routes.map(({ to, name }) => (
                                     <li key={to}>
                                         <NavLink
                                             to={to}
@@ -35,7 +35,7 @@ export const Navigation = () => {
                             </ul>
                         </nav>
                         <Routes>
-                            {routes.map(({to, path, Component}) => <Route key={to} path={path} element={<Component/>}/>)}
+                            {routes.map(({ to, path, Component }) => <Route key={to} path={path} element={<Component />} />)}
                             <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
                         </Routes>
                     </div>
