cb23534b6b4f825af24490fa4e3d8401df97d766
Hazael Lopez <elhazael134@gmail.com>
2023-08-13 22:03:06 -0600

    Componente básico tradicional

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,65 +1,65 @@
 import {
-         BrowserRouter,
-         NavLink,
-         Navigate,
-         Route,
-         Routes } from 'react-router-dom';
+    BrowserRouter,
+    NavLink,
+    Navigate,
+    Route,
+    Routes
+} from 'react-router-dom';
 import logo from '../logo.svg';
 import { Suspense } from 'react';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 
 
 export const Navigation = () => {
     return (
         <>
-            <Suspense fallback={<span>Loading...</span>}>
-                <BrowserRouter>
-                    <div className='main-layout'>
-                        <nav>
-                            <img src={logo} alt='react logo' />
-                            <ul>
-                                <li>
-                                    <NavLink 
-                                        to="/" 
-                                        className={
-                                            ({isActive}) => isActive ? 'nav-active': ''
-                                            }
-                                    >     
-                                    Home   
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink 
-                                        to="/" 
-                                        className={
-                                            ({isActive}) => isActive ? 'nav-active': ''
-                                            }
-                                    >     
-                                    User   
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink 
-                                        to="/" 
-                                        className={
-                                            ({isActive}) => isActive ? 'nav-active': ''
-                                            }
-                                    >   
-                                    About     
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </nav>
-                        <Routes>
-                            <Route path="about" element={<h1>About page</h1>}/>
-                            <Route path="user"  element={<h1>User page</h1>}/>
-                            <Route path="home"  element={<h1>Home page</h1>}/>
+            <BrowserRouter>
+                <div className='main-layout'>
+                    <nav>
+                        <img src={logo} alt='react logo' />
+                        <ul>
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={
+                                        ({ isActive }) => isActive ? 'nav-active' : ''
+                                    }
+                                >
+                                    Shopping
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={
+                                        ({ isActive }) => isActive ? 'nav-active' : ''
+                                    }
+                                >
+                                    User
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={
+                                        ({ isActive }) => isActive ? 'nav-active' : ''
+                                    }
+                                >
+                                    About
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<ShoppingPage />} />
+                        <Route path="/about" element={<h1>About page</h1>} />
+                        <Route path="/user" element={<h1>User page</h1>} />
 
-                            <Route path='/*' element={<Navigate to="/home" replace />} />
-                        </Routes>
-                    </div>
-                </BrowserRouter>
-            </Suspense>
+                        <Route path='/*' element={<Navigate to="/home" replace />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </>
     )
 }
