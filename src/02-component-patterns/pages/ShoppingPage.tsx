8eccd86a565bf60a4921b3c0f5bd71780fa16349
Hazael Lopez <elhazael134@gmail.com>
2023-08-13 23:16:33 -0600

    Recibir props al componente hijo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,13 +1,26 @@
 import { ProductCard } from "../components/ProductCard"
 
 
+const product = {
+    id: '1',
+    title: 'Coffe Mug - Card',
+    img: './coffee-mug.png'
+}
+
+
 export const ShoppingPage = () => {
   return (
     <>
       <div>
         <h1>Shopping Store</h1>
         <hr/>
-        <ProductCard/>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap'
+        }}>
+          <ProductCard product={product} />
+        </div>
       </div>
     </>
   )
